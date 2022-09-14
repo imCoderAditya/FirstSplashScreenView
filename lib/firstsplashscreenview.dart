e6701ab176c75a38633933b6a00f8bcc348a6198
imCoderAditya <adsharmavashishtha@gmail.com>
2022-09-14 12:28:52 +0530

    Release 0.0.4

--- lib/firstsplashscreenview.dart
+++ lib/firstsplashscreenview.dart
@@ -7,32 +7,28 @@ class SplashScreen extends StatefulWidget {
   Icon? icon;
   Text? text;
   Widget? nextPage;
-  CircleAvatar? circleAvatar;
   Timer? timer;
   Gradient? gradient;
-  Navigator? navigator;
   AppBar? appBar;
   Color? appBarBackgroundColor;
   Duration? duration;
 
-  SplashScreen(
-      {super.key,
-      this.duration,
-      this.child,
-      this.navigator,
-      this.circleAvatar,
-      this.colors,
-      this.gradient,
-      this.appBar,
-      required this.nextPage,
-      this.icon,
-      this.timer,
-      this.text})
-      : assert(child == null ||
+  SplashScreen({
+    super.key,
+    this.duration = const Duration(seconds: 5),
+    this.child,
+    this.colors = Colors.red,
+    this.gradient,
+    this.appBar,
+    required this.nextPage,
+    this.icon,
+    this.timer,
+    this.text,
+  })  : assert(child == null ||
             appBar == null ||
             timer == null ||
             duration == null),
-        assert(circleAvatar == null || colors == null || icon == null);
+        assert(colors == null || icon == null);
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
@@ -66,7 +62,6 @@ class _SplashScreenState extends State<SplashScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                widget.circleAvatar!,
                 Container(
                   child: widget.child,
                 )
